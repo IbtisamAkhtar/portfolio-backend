17319212dafd7be60296b9e95549c3583c615974
IbtisamAkhtar <akhteribtisam@gmail.com>
2025-09-22 21:05:18 +0500

    Fix port configuration with Procfile

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,12 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Laravel Portfolio Backend is working!',
+        'status' => 'success'
+    ]);
+});
+
+Route::get('/test', function () {
+    return 'Simple test endpoint working!';
 });
